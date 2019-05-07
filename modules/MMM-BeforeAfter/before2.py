a621f61ea66c230ee7cfc0fd81690b595d8cba0e
emrhssla <emrhssla@gmail.com>
2019-05-07 19:35:43 +0100

    version1.3

--- modules/MMM-BeforeAfter/before2.py
+++ modules/MMM-BeforeAfter/before2.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+import datetime
 import cv2
 
 cap = cv2.VideoCapture(0)
-
+cap.set(3,640)
+cap.set(4,480)
 ret, frame = cap.read()
+now = datetime.datetime.now().strftime("%d_%H-%M-%S")
 cv2.imshow('frame', frame)
 cv2.imwrite('/home/pi/BeautyM/modules/MMM-BeforeAfter/before/before2.jpg', frame)
+cv2.imwrite("/home/pi/BeautyM/modules/MMM-BeforeAfter/minsoo/" + str(now) + ".png", frame)
 
 cap.release()
 cv2.destroyAllWindows()
