db480f8f7515e20aebe62670de263ac7113a90f1
emrhssla <emrhssla@gmail.com>
2019-06-17 15:20:02 +0900

    History HEAD adding 2019- , DeleteButtom HTML

--- modules/MMM-HistoryImage1/MMM-HistoryImage1.js
+++ modules/MMM-HistoryImage1/MMM-HistoryImage1.js
@@ -394,7 +394,7 @@ Module.register("MMM-HistoryImage1", {
 		}
 		else
 		{
-			return this.config.c[1]+"날 컷트 사진입니다.";
+			return "2019-"+this.config.c[1]+"날 컷트 사진입니다.";
 		}
 		//return '2019-05-'+this.data.header;
 	},
