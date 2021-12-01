c60e1d7043e8ace245882dd412529960ff34ca72
emrhssla <emrhssla@gmail.com>
2021-12-02 02:14:42 +0900

    Make CategoryChoiceYoutube

--- modules/CategoryChoiceYoutube/CategoryChoiceYoutube.js
+++ modules/CategoryChoiceYoutube/CategoryChoiceYoutube.js
@@ -119,7 +119,143 @@ Module.register("CategoryChoiceYoutube",{
 		item.className = "modulebar-button";
 		item.style.minWidth = self.config.minWidth;
         item.style.minHeight = self.config.minHeight;
-		
+		// 모든 모듈 불러오기
+		var modules = MM.getModules();
+		item.addEventListener("click", function () {
+			// 모듈을 하나하나 모두 검사
+			for (var i = 0; i < modules.length; i++) {
+				// 원하는 모듈을 찾았을때
+				if (modules[i].name === data.module) {
+					// 해당 모듈 데이터 split
+					var idnr = modules[i].data.identifier.split("_");					
+					if (idnr[1] == data.idnum || data.idnum == null) {
+						// 원하는 모듈이 hide일때
+						if (modules[i].hidden) {
+							if (data.showUrl != null) {
+								fetch(data.showUrl);
+								console.log("Visiting show URL: "+data.showUrl);
+							}
+							// EmbedYoutube 에게  출력할 주제를 신호로 보낸다.
+							if(num==1){
+								CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"YCcE9oGkOw8_롤");			
+									} 
+							else	if(num==2){
+										CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"ScSn235gQx0_뮤직비디오");			
+											} 
+							else	if(num==3){
+								CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"wzKcDqRcIx0_영화");			
+									} 
+							else	if(num==4){
+								CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"L0oei9OH7Yo_뉴스");			
+									}
+							else	if(num==5){
+								CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"Bxg1CqqkzE0_스포츠");			
+									}
+							else	if(num==6){
+								CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"egyB02dbJKE_먹방");			
+									}
+							else	if(num==7){
+								CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"zOQ_t2dCMq0_해외여행");			
+									}
+							else	if(num==8){
+								CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"MFWtM11WJn0_몰카");			
+									}
+							// 모듈 보여주기
+							modules[i].show(self.config.animationSpeed, {force: self.config.allowForce});
+							console.log("Showing "+modules[i].name+" ID: "+idnr[1]);
+						}
+						//모듈이 이미 켜져있는 경우
+						else{
+							// 이미 선택된 주제로 켜져있는 유튜브일 경우 꺼준다
+							if(num==1){
+								if (modules[i].config.video_id == "YCcE9oGkOw8") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								//아닐경우 해당 주제의 유튜브를 켜준다. 나머지는 전부 꺼준다.
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"YCcE9oGkOw8_롤");			
+									} 
+							else	if(num==2){
+								if (modules[i].config.video_id == "ScSn235gQx0") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"ScSn235gQx0_뮤직비디오");	
+											
+									} 
+							else	if(num==3){
+								if (modules[i].config.video_id == "KUiouwhozkQ") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"KUiouwhozkQ_영화");	
+											
+									} 
+							else	if(num==4){
+								if (modules[i].config.video_id == "L0oei9OH7Yo") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"L0oei9OH7Yo_뉴스");	
+											
+											} 
+							else	if(num==5){
+								if (modules[i].config.video_id == "Bxg1CqqkzE0") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"Bxg1CqqkzE0_스포츠");	
+											
+											} 
+							else	if(num==6){
+								if (modules[i].config.video_id == "egyB02dbJKE") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"egyB02dbJKE_먹방");	
+											
+											} 
+							else	if(num==7){
+								if (modules[i].config.video_id == "Vw39vVf2HCI") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"Vw39vVf2HCI_해외여행");	
+											
+											} 
+							else	if(num==8){
+								if (modules[i].config.video_id == "MFWtM11WJn0") {
+									                                console.log("modules[i].config.video_id ?"+ modules[i].config.video_id);
+																	modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                                //modules[5].hide(self.config.animationSpeed, {force: self.config.allowForce});
+									                            }
+								else
+									CategoryChoiceYoutube.sendNotification('PLAYLISTCHANGE',"MFWtM11WJn0_몰카");	
+											
+											} 
+							if (data.hideUrl != null) {
+								fetch(data.hideUrl);
+								console.log("Visiting hide URL: "+data.hideUrl);
+							}
+						}
+					}
+				}
+			}
+		});
 		// Fixes the aligning.
         item.style.flexDirection = {
             "right"  : "row-reverse",
@@ -177,8 +313,11 @@ Module.register("CategoryChoiceYoutube",{
 		// All done. :)
         return item;
 	},
-	
-	
+	notificationReceived: function(notification, payload) {
+		if(notification === 'Modules All Change'){
+			this.hide()
+		}
+	}
 	
 });	
 
