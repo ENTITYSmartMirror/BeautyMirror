4e34dd5b4e04d849165aaea84b6cf3901cc20d25
emrhssla <emrhssla@gmail.com>
2021-12-02 02:40:49 +0900

    Make CategoryChoiceEntMenu

--- modules/CategoryChoiceEntMenu/CategoryChoiceEntMenu.js
+++ modules/CategoryChoiceEntMenu/CategoryChoiceEntMenu.js
@@ -32,7 +32,18 @@ Module.register("CategoryChoiceEntMenu",{
 		animationSpeed: 500,
         // The default button 1. Add your buttons in the config.
         buttons: {
-                    
+                    "1": {
+	                  		module: "ShowWebtoon",
+												img:"https://apprecs.org/gp/images/app-icons/300/ec/com.nhn.android.webtoon.jpg",
+												width:"50",
+												height:"50",
+                          },
+										"2": {
+												module: "CategoryChoiceYoutube",
+												img:"https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-256.png",
+												width:"50",
+												height:"50",
+											},
             }
     },
 
@@ -54,8 +65,127 @@ Module.register("CategoryChoiceEntMenu",{
 
         return menu;
     },
+	// 버튼 생성
+    createButton: function (self, num, data, placement) {
+		// span Element 생성
+		var item = document.createElement("span");
+        // 버튼 id 설정
+		item.id = self.identifier + "_button_" + num;
+        // 버튼 클래스 지정
+		item.className = "modulebar-button";
+		// 버튼 최소의 넓이 높이 지정.
+		item.style.minWidth = self.config.minWidth;
+        item.style.minHeight = self.config.minHeight;
+		// 매직미러에 로드된 모든 모듈들을 호출
+		var modules = MM.getModules();
+		// 클릭시
+		item.addEventListener("click", function () {
+			// 모든 모듈체크
+			for (var i = 1; i < modules.length; i++) {
+				// 현재 모듈 확인
+				if (modules[i].name === data.module) {
+					var idnr = modules[i].data.identifier.split("_");
+					console.log("idnr : "+idnr+"idnum"+data.idnum);
+					if (idnr[1] == data.idnum || data.idnum == null) {
+						// 모듈이 숨겨져있는 상태일때
+						if (modules[i].hidden) {
+							// Showurl 설정 확인
+							if (data.showUrl != null) {
+								//Show url
+								fetch(data.showUrl);
+								console.log("Visiting show URL: "+data.showUrl);
+							}
+							//한 프레임에 두가지이상의 모듈이 뜨지 않게 하기.
+							
+							if (modules[i].name == 'ShowWebtoon') {
+								console.log("Hiding opend "+ modules[i].name+" ID: "+idnr[1]+"button number"+num);
+								modules[26].hide(self.config.animationSpeed, {force: self.config.allowForce});
+								modules[28].hide(self.config.animationSpeed, {force: self.config.allowForce});
+								setTimeout(function(){
+									modules[25].show(self.config.animationSpeed, {force: self.config.allowForce});
+								},500);
+							}
+							else if (modules[i].name == 'CategoryChoiceYoutube') {
+								console.log("Hiding opend "+ modules[i].name+" ID: "+idnr[1]+"button number"+num);
+								modules[25].hide(self.config.animationSpeed, {force: self.config.allowForce});
+								modules[26].hide(self.config.animationSpeed, {force: self.config.allowForce});
+								setTimeout(function(){
+									modules[28].show(self.config.animationSpeed, {force: self.config.allowForce});
+								},500);
+							}
+						}else{
+							// 모듈이 켜있는 상태일때 
+							modules[26].hide(self.config.animationSpeed, {force: self.config.allowForce});
+							modules[i].hide(self.config.animationSpeed, {force: self.config.allowForce});
+							console.log("Hiding "+modules[i].name+" ID: "+idnr[1]);
+							// hideURL이 설정되 있을 때
+							if (data.hideUrl != null) {
+								fetch(data.hideUrl);
+								console.log("Visiting hide URL: "+data.hideUrl);
+							}
+						}
+					}
+				}
+			}
+		});
 
-	
+		// 버튼 배열
+        item.style.flexDirection = {
+            "right"  : "row-reverse",
+            "left"   : "row",
+            "top"    : "column",
+            "bottom" : "column-reverse"
+        }[placement];
+		// 테두리설정
+        if (!self.config.showBorder) {
+            item.style.borderColor = "black";
+        }
+		// Adds the Font-Awesome symbol if specified.
+        if (data.symbol) {
+            var symbol = document.createElement("span");
+            symbol.className = "modulebar-picture fa fa-" + data.symbol;
+			// Sets the size on the symbol if specified.
+            if (data.size) {
+                symbol.className += " fa-" + data.size;
+                symbol.className += data.size == 1 ? "g" : "x";
+            }
+			// Align the symbol with a margin.
+            if (data.text && placement === "left") {
+                symbol.style.marginRight = "4px";
+            }
+			// Adds the symbol to the item.
+            item.appendChild(symbol);
+
+		// Adds a picture if specified.
+		} else if (data.img) {
+            var image = document.createElement("img");
+            image.className = "modulebar-picture";
+            image.src = data.img;
+			// Sets the size of the picture if specified.
+            if (data.width)  image.width  = data.width;
+            if (data.height) image.height = data.height;
+			// Align the picture with a margin.
+            if (data.text && placement === "left") {
+                image.style.marginRight = "4px";
+            }
+			// Adds the picture to the item.
+            item.appendChild(image);
+        }
+		// Adds the text if specified.
+        if (data.text) {
+            var text = document.createElement("span");
+            text.className = "modulebar-text";
+            text.innerHTML = data.text;
+			// Align the text with a margin.
+            if ((data.symbol || data.img) && placement === "right") {
+                text.style.marginRight = "4px";
+            }
+			// Adds the text to the item.
+            item.appendChild(text);
+        }
+		// All done. :)
+        return item;
+	},
 });	
 
 
