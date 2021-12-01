757a6ad39e54b34aa52f17b71c278dda256f354d
emrhssla <emrhssla@gmail.com>
2021-12-02 00:46:52 +0900

    BeforeAfter.js 정리

--- modules/MMM-BeforeAfter/MMM-BeforeAfter.js
+++ modules/MMM-BeforeAfter/MMM-BeforeAfter.js
@@ -27,39 +27,39 @@ Module.register("MMM-BeforeAfter", {
   
   notificationReceived: function(notification, payload, sender) {
     switch(notification) {
+      // BeforeImage모듈로부터 BEFOREIMAGECLICK 신호를 받았을때
       case "BEFOREIMAGECLICK" :
         var baelem = document.getElementById("BeforeAfterClickid")     
+          // 사진찍기 python shell 작동시작
           BeforeAfterMoudule.sendSocketNotification("BEFORECAPTURE")
           //baelem.innerHTML = "카메라 로딩 중"       
         break;
+      // BeforeImage모듈로부터 BEFOREIMAGECLICK 신호를 받았을때
       case "AFTERIMAGECLICK" :
       var baelem2 = document.getElementById("BeforeAfterClickid2")
+        // 사진찍기 python shell 작동시작
         BeforeAfterMoudule.sendSocketNotification("AFTERCAPTURE")
-        //BeforeAfterMoudule.sendNotification("LOADINGAFTER")
         //baelem2.innerHTML = "카메라 로딩 중"       
         break;
     }
   },
   socketNotificationReceived: function(notification, payload) {
     switch(notification) {
+      // BeforeImage사진찍기 성공
       case "BEFORECAPTURESUCCESS":
         console.log("Socket recevied payload1: "+payload)
         var baelem = document.getElementById("BeforeAfterClickid")
+        //BeforeImage 모듈에게 사진이 찍혔다고 신호전달
         BeforeAfterMoudule.sendNotification("BEFOREIMAGE")
-        //
-        BeforeAfterMoudule.sendNotification('SHOWCHANGEDIMAGE');
-        //
         //baelem.innerHTML = "자르기 전 사진찍기 완료!"
         break
+      // AfterImage사진찍기 성공
       case "AFTERCAPTURESUCCESS":
         console.log("Socket recevied payload1: "+payload)
         var baelem2 = document.getElementById("BeforeAfterClickid2")
+        // //AfterImage 모듈에게 사진이 찍혔다고 신호전달
         BeforeAfterMoudule.sendNotification("AFTERIMAGE")
-        //
-        BeforeAfterMoudule.sendNotification('SHOWCHANGEDIMAGE');
-        //
         BeforeAfterMoudule.sendNotification("CUTDAY",payload)
-
         //baelem2.innerHTML = "자르기 후 사진찍기 완료!"
       break
     }
--- modules/MMM-BeforeAfter/node_helper.js
+++ modules/MMM-BeforeAfter/node_helper.js
@@ -9,6 +9,7 @@ module.exports = NodeHelper.create({
   
   socketNotificationReceived: function(notification, payload) {
     switch(notification) {
+      // BeforeImage 사진 촬영 시잦ㄱ
       case "BEFORECAPTURE":
         console.log("notification : " + notification)
 	      PythonShell.run('./modules/MMM-BeforeAfter/before.py', null, function (err, result) {
@@ -16,8 +17,8 @@ module.exports = NodeHelper.create({
             console.log(result);          
             socketTestpython.sendSocketNotification("BEFORECAPTURESUCCESS",result);
           });
-	       
         break
+      // AfterImage 사진 촬영 시잦ㄱ
       case "AFTERCAPTURE":
         console.log("notification : " + notification)
         PythonShell.run('./modules/MMM-BeforeAfter/before2.py', null, function (err, result) {
