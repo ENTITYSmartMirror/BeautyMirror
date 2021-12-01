9e6e839966da360432b0360db426cd97fab1c120
emrhssla <emrhssla@gmail.com>
2021-12-01 23:24:14 +0900

    Make BeforeImage

--- modules/MMM-BeforeImage/MMM-BeforeImage.js
+++ modules/MMM-BeforeImage/MMM-BeforeImage.js
@@ -11,14 +11,17 @@
  * Module MMM-ImageSlideshow By Adam Moses http://adammoses.com
  * MIT Licensed.
  */
+
  
-Module.register("MMM-ImageSlideshow", {
+var BeforeImages;
+Module.register("MMM-BeforeImage", {
+
 	// Default module config.
 	defaults: {
         // an array of strings, each is a path to a directory with images
         imagePaths: [ 'modules/MMM-ImageSlideshow/exampleImages' ],
         // the speed at which to switch between images, in milliseconds
-		slideshowSpeed: 10 * 1000,
+		slideshowSpeed: 10 * 50,
         // if zero do nothing, otherwise set width to a pixel value
         fixedImageWidth: 0,
         // if zero do nothing, otherwise set height to a pixel value        
@@ -28,15 +31,20 @@ Module.register("MMM-ImageSlideshow", {
         // if true combine all images in all the paths
         // if false each path with be viewed seperately in the order listed
         treatAllPathsAsOne: false,
+		// 이미지 reload시 파일리스트
+		reloadImageList: true,
         // if true, all images will be made grayscale, otherwise as they are
         makeImagesGrayscale: false,
         // list of valid file extensions, seperated by commas
         validImageFileExtensions: 'bmp,jpg,gif,png',
 		// a delay timer after all images have been shown, to wait to restart (in ms)
 		delayUntilRestart: 0,
+		a:0,
+		complete:0
 	},
     // load function
 	start: function () {
+		BeforeImages = this;
         // add identifier to the config
         this.config.identifier = this.identifier;
         // ensure file extensions are lower case
@@ -44,7 +52,7 @@ Module.register("MMM-ImageSlideshow", {
         // set no error
 		this.errorMessage = null;
         if (this.config.imagePaths.length == 0) {
-            this.errorMessage = "MMM-ImageSlideshow: Missing required parameter."
+			this.errorMessage = "MMM-ImageSlideshow: Missing required parameter."
         }
         else {
             // create an empty image list
@@ -52,11 +60,14 @@ Module.register("MMM-ImageSlideshow", {
             // set beginning image index to -1, as it will auto increment on start
             this.imageIndex = -1;
             // ask helper function to get the image list
+            console.log("MMM-ImageSlideshow sending socket notification");
             this.sendSocketNotification('IMAGESLIDESHOW_REGISTER_CONFIG', this.config);
 			// do one update time to clear the html
 			this.updateDom();
 			// set a blank timer
 			this.interval = null;
+			// 동적으로 사진띄우기 위해 loaded 설정
+			this.loaded = false;
         }
 	},
 	// Define required scripts.
@@ -66,16 +77,29 @@ Module.register("MMM-ImageSlideshow", {
 	},    
 	// the socket handler
 	socketNotificationReceived: function(notification, payload) {
+                console.log("MMM-ImageSlideshow recieved a socket notification: " + notification);
 		// if an update was received
 		if (notification === "IMAGESLIDESHOW_FILELIST") {
 			// check this is for this module based on the woeid
 			if (payload.identifier === this.identifier)
 			{
+				var newImageList = payload.imageList;
+				if (newImageList.length == this.imageList.length) {
+					var unchanged = true;
+					for (var i = 0 ; i < newImageList.length; i++) {
+						unchanged = this.imageList[i] == newImageList[i];
+						if (!unchanged)
+							break;
+					}
+					if (unchanged)
+						return;
+				}
 				// set the image list
 				this.imageList = payload.imageList;
                 // if image list actually contains images
                 // set loaded flag to true and update dom
-                if (this.imageList.length > 0) {
+				// 새로운 사진 Update
+                if (this.imageList.length > 0 && !this.loaded) {
                     this.loaded = true;
                     this.updateDom();
 					// set the timer schedule to the slideshow speed			
@@ -113,6 +137,9 @@ Module.register("MMM-ImageSlideshow", {
 				var showSomething = true;
                 // if exceeded the size of the list, go back to zero
                 if (this.imageIndex == this.imageList.length) {
+                                       // 이미지 reload
+                                       if (this.config.reloadImageList) 
+                                           this.sendSocketNotification('IMAGESLIDESHOW_RELOAD_FILELIST', this.config);
 					// if delay after last image, set to wait
 					if (this.config.delayUntilRestart > 0) {
 						this.imageIndex = -2;
@@ -131,7 +158,16 @@ Module.register("MMM-ImageSlideshow", {
 				if (showSomething) {
 					// create the image dom bit
 					var image = document.createElement("img");
-					// if set to make grayscale, flag the class set in the .css file
+					image.id="imgid";
+					// 'Before사진 찍기' 시작하기 클릭시
+					image.addEventListener("click", () => {
+						BeforeImages.sendNotification("camera_stop")
+						console.log(" image click !!!!!");
+						// 로딩중 화면을 띄우기
+						this.config.a=3;
+						// BeforeAfter 모듈에게 python으로 사진찍기 위한 신호 전달
+						BeforeImages.sendNotification("BEFOREIMAGECLICK");
+                        });
 					if (this.config.makeImagesGrayscale)
 						image.className = "desaturate";
 					// create an empty string
@@ -144,9 +180,25 @@ Module.register("MMM-ImageSlideshow", {
 					// if style string has antyhing, set it
 					if (styleString != '')
 						image.style = styleString;
-					// set the image location
-					image.src = encodeURI(this.imageList[this.imageIndex]);
-					// ad the image to the dom
+					//imgaeList = modules/MMM-BeforeAfter/before 저장해놓은 사진 load
+					//클릭해주세요 이미지
+					if(this.config.a==0){
+					image.src = this.imageList[0];
+					}
+					// 찍혀진 사진 출력
+					if(this.config.a==1){
+						image.src = this.imageList[this.imageList.length-1];
+						}
+					// AfterImage 사진 찍기 시작 이미지 버튼
+					if(this.config.a==2){
+						image.src = this.imageList[this.imageList.length-2];
+						this.config.complete=1;
+						BeforeImages.sendNotification("camera_start");
+						}
+					// 로딩이미지
+					if(this.config.a==3){
+						image.src = this.imageList[2];
+						}
 					wrapper.appendChild(image);					
 				}
             }
@@ -157,5 +209,36 @@ Module.register("MMM-ImageSlideshow", {
         }
         // return the dom
 		return wrapper;
+	},
+	notificationReceived: function(notification, payload) {
+		Log.info(this.name + " - received notification: " + notification);
+		if(notification === "DOM_OBJECTS_CREATED"){
+			// 모듈이 시작할땐 모든 사진 초기화
+			BeforeImages.sendNotification("DELETEstart");
+		}
+		// 사진데이터 초기화와 모든 모듈 재시작
+		if(notification === "Modules All Change"){
+			if(BeforeImages.config.complete===1){
+				console.log("complete what :"+BeforeImages.config.complete);
+				BeforeImages.sendNotification("DELETEstart");
+				BeforeImages.config.complete=0;
+			}
+		}
+		// beforeimage가 찍혔을때
+		if(notification === "BEFOREIMAGE"){
+			this.config.a=1;
+		}
+		// 기본값으로 돌아가기
+		if(notification === "setDefault"){
+			this.config.a=0;
+		}
+		// Afterimage
+		if(notification === "AFTERIMAGE"){
+			this.config.a=2;
+		}
+		//로딩중이미지 출력
+		if(notification === "LOADINGBEFORE"){
+			this.config.a=3;
+		}
 	}
 });
--- modules/MMM-BeforeImage/node_helper.js
+++ modules/MMM-BeforeImage/node_helper.js
@@ -21,6 +21,7 @@ module.exports = NodeHelper.create({
     // subclass start method, clears the initial config array
     start: function() {
         this.moduleConfigs = [];
+        console.log("Starting Node Helper for: " + this.name);
     },
     // shuffles an array at random and returns it
     shuffleArray: function(array) {
@@ -105,6 +106,7 @@ module.exports = NodeHelper.create({
     },
     // subclass socketNotificationReceived, received notification from module
     socketNotificationReceived: function(notification, payload) {
+        //console.log("MMM-ImageSlideshow node_helper received notification");
         if (notification === "IMAGESLIDESHOW_REGISTER_CONFIG") {
             // add the current config to an array of all configs used by the helper
             this.moduleConfigs.push(payload);
@@ -117,7 +119,12 @@ module.exports = NodeHelper.create({
             // send the image list back
             self.sendSocketNotification('IMAGESLIDESHOW_FILELIST', returnPayload );
         }
-    },     
+        else if (notification === "IMAGESLIDESHOW_RELOAD_FILELIST") {
+            var imageList = this.gatherImageList(this.moduleConfigs[this.moduleConfigs.length - 1]);
+            var returnPayload = { identifier: payload.identifier, imageList: imageList };
+            this.sendSocketNotification('IMAGESLIDESHOW_FILELIST', returnPayload );
+        }
+    },
 });
 
 //------------ end -------------
