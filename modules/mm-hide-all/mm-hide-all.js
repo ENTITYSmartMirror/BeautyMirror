77de8fd74ef3dce97055884dd740117ed43df28f
emrhssla <emrhssla@gmail.com>
2021-12-01 14:52:28 +0900

    default hide all module

--- modules/mm-hide-all/mm-hide-all.js
+++ modules/mm-hide-all/mm-hide-all.js
@@ -6,12 +6,9 @@
  * By EoF https://forum.magicmirror.builders/user/eof
  * MIT Licensed.
  */
-var hideall;
+
 Module.register("mm-hide-all",{
-	defaults: {},
-	start: function(){
-		hideall=this;
-	},
+
 	getScripts: function() {
 		return ["modules/mm-hide-all/js/jquery.js"];
 	},
@@ -27,7 +24,7 @@ Module.register("mm-hide-all",{
 		var overlay = document.createElement("div");
 		var hidden = true;
 		
-		//overlay.className = "paint-it-black";
+		overlay.className = "paint-it-black";
 		
 		button.className = "hide-toggle";
 		button.appendChild(text);
@@ -38,34 +35,16 @@ Module.register("mm-hide-all",{
 		
 		$(button).on("click", function(){
 			if(hidden){
-				hideall.sendNotification("only_camera");
-				/*
-				hideall.sendNotification('CHANGE_POSITIONS', 
-					modules = {
-						'clock':{
-							visible: 'true',
-							position: 'top_right',
-						}
-					}
-				);
-				*/
-				//$(overlay).fadeIn(1000);
+				$(overlay).fadeIn(1000);
 				$(button).fadeTo(1000, 0.3);
 				$(text).html('Show');
 				hidden = false;
-				
 			}else{
-				//$(overlay).fadeOut(1000);
-				hideall.sendNotification("show_camera");
-				
+				$(overlay).fadeOut(1000);
 				$(button).fadeTo(1000, 1);
 				$(text).html('Hide');
 				hidden = true;
-				
-				
 			}
-			
-				console.log("camera start");
 		});
 		
 		return wrapper;
