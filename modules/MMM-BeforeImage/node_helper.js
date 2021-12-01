b6df4e02dde055f7e8137539df5a8e2f82214409
emrhssla <emrhssla@gmail.com>
2021-12-01 21:26:45 +0900

    default BefroeImage

--- modules/MMM-BeforeImage/node_helper.js
+++ modules/MMM-BeforeImage/node_helper.js
@@ -1,6 +1,6 @@
 /* global Module */
 
-/* MMM-ImageSlideshow.js
+/* node_helper.js
  * 
  * Magic Mirror
  * Module: MMM-ImageSlideshow
@@ -11,151 +11,113 @@
  * Module MMM-ImageSlideshow By Adam Moses http://adammoses.com
  * MIT Licensed.
  */
- 
-Module.register("MMM-ImageSlideshow", {
-	// Default module config.
-	defaults: {
-        // an array of strings, each is a path to a directory with images
-        imagePaths: [ 'modules/MMM-ImageSlideshow/exampleImages' ],
-        // the speed at which to switch between images, in milliseconds
-		slideshowSpeed: 10 * 1000,
-        // if zero do nothing, otherwise set width to a pixel value
-        fixedImageWidth: 0,
-        // if zero do nothing, otherwise set height to a pixel value        
-        fixedImageHeight: 0,
-        // if true randomize image order, otherwise do alphabetical
-        randomizeImageOrder: false,
-        // if true combine all images in all the paths
-        // if false each path with be viewed seperately in the order listed
-        treatAllPathsAsOne: false,
-        // if true, all images will be made grayscale, otherwise as they are
-        makeImagesGrayscale: false,
-        // list of valid file extensions, seperated by commas
-        validImageFileExtensions: 'bmp,jpg,gif,png',
-		// a delay timer after all images have been shown, to wait to restart (in ms)
-		delayUntilRestart: 0,
-	},
-    // load function
-	start: function () {
-        // add identifier to the config
-        this.config.identifier = this.identifier;
-        // ensure file extensions are lower case
-        this.config.validImageFileExtensions = this.config.validImageFileExtensions.toLowerCase();
-        // set no error
-		this.errorMessage = null;
-        if (this.config.imagePaths.length == 0) {
-            this.errorMessage = "MMM-ImageSlideshow: Missing required parameter."
+
+// call in the required classes
+var NodeHelper = require("node_helper");
+var FileSystemImageSlideshow = require("fs");
+
+// the main module helper create
+module.exports = NodeHelper.create({
+    // subclass start method, clears the initial config array
+    start: function() {
+        this.moduleConfigs = [];
+    },
+    // shuffles an array at random and returns it
+    shuffleArray: function(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    },
+    // sort by filename attribute
+    sortByFilename: function (a, b) {
+        aL = a.filename.toLowerCase();
+        bL = b.filename.toLowerCase();
+        if (aL > bL) 
+			return 1;
+		else 
+			return -1;
+    },
+    // checks there's a valid image file extension
+    checkValidImageFileExtension: function(filename, extensions) {
+        var extList = extensions.split(',');
+        for (var extIndex = 0; extIndex < extList.length; extIndex++) {
+            if (filename.toLowerCase().endsWith(extList[extIndex]))
+                return true;
         }
-        else {
-            // create an empty image list
-            this.imageList = [];
-            // set beginning image index to -1, as it will auto increment on start
-            this.imageIndex = -1;
-            // ask helper function to get the image list
-            this.sendSocketNotification('IMAGESLIDESHOW_REGISTER_CONFIG', this.config);
-			// do one update time to clear the html
-			this.updateDom();
-			// set a blank timer
-			this.interval = null;
-        }
-	},
-	// Define required scripts.
-	getStyles: function() {
-        // the css contains the make grayscale code
-		return ["imageslideshow.css"];
-	},    
-	// the socket handler
-	socketNotificationReceived: function(notification, payload) {
-		// if an update was received
-		if (notification === "IMAGESLIDESHOW_FILELIST") {
-			// check this is for this module based on the woeid
-			if (payload.identifier === this.identifier)
-			{
-				// set the image list
-				this.imageList = payload.imageList;
-                // if image list actually contains images
-                // set loaded flag to true and update dom
-                if (this.imageList.length > 0) {
-                    this.loaded = true;
-                    this.updateDom();
-					// set the timer schedule to the slideshow speed			
-					var self = this;
-					this.interval = setInterval(function() {
-						self.updateDom();
-						}, this.config.slideshowSpeed);					
+        return false;
+    },
+    // gathers the image list
+    gatherImageList: function(config) {
+        var self = this;
+        // create an empty main image list
+        var imageList = [];
+        // for each of the paths specified
+        for (var pathIndex = 0; pathIndex < config.imagePaths.length; pathIndex++) {
+            var currentPath = config.imagePaths[pathIndex];
+            var currentPathImageList = FileSystemImageSlideshow.readdirSync(path = currentPath);
+            // for each file in the current path
+            if (currentPathImageList.length > 0) {
+                // create an empty list for images in the current path
+                var currentImageList = [];
+                // for each file
+                for (var imageIndex = 0; imageIndex < currentPathImageList.length; imageIndex++) {
+                    // seperate into path and filename
+                    var currentImage = {path: currentPath, filename: currentPathImageList[imageIndex]};
+                    // check if file has a valid image file extension
+                    var isValidImageFileExtension = this.checkValidImageFileExtension(
+                                currentImage.filename, 
+                                config.validImageFileExtensions);
+                    //  if file is valid, add it to the list
+                    if (isValidImageFileExtension)
+                        currentImageList.push(currentImage);
                 }
-			}
+                // if not set to combine all paths, do random or alphabetical sort
+                if (!config.treatAllPathsAsOne) {
+                    if (config.randomizeImageOrder)
+                        currentImageList = this.shuffleArray(currentImageList);
+                    else
+                        currentImageList = currentImageList.sort(this.sortByFilename);
+                }
+                // add current list main list
+                imageList = imageList.concat(currentImageList);
+            }
+        }
+        // if set to combine all paths, sort all images randomly or alphabetically by filename
+        if (config.treatAllPathsAsOne) {
+            if (config.randomizeImageOrder)
+                imageList = this.shuffleArray(imageList);
+            else
+                imageList = imageList.sort(this.sortByFilename);
+        }
+        // create a file image list combining paths and filenames
+        var imageListComplete = [];
+        for (var index = 0; index < imageList.length; index++) {
+            imageListComplete.push(imageList[index].path + '/' + imageList[index].filename);
 		}
-    },    
-	// Override dom generator.
-	getDom: function () {
-		var wrapper = document.createElement("div");
-        // if an error, say so (currently no errors can occur)
-        if (this.errorMessage != null) {
-            wrapper.innerHTML = this.errorMessage;
+        // return final list
+        return imageListComplete;
+    },
+    // subclass socketNotificationReceived, received notification from module
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "IMAGESLIDESHOW_REGISTER_CONFIG") {
+            // add the current config to an array of all configs used by the helper
+            this.moduleConfigs.push(payload);
+            // this to self
+            var self = this;
+            // get the image list
+            var imageList = this.gatherImageList(payload);
+            // build the return payload
+            var returnPayload = { identifier: payload.identifier, imageList: imageList };
+            // send the image list back
+            self.sendSocketNotification('IMAGESLIDESHOW_FILELIST', returnPayload );
         }
-        // if no errors
-        else {
-            // if the image list has been loaded
-            if (this.loaded === true) {
-				// if was delayed until restart, reset index reset timer
-				if (this.imageIndex == -2) {
-					this.imageIndex = -1;
-					clearInterval(this.interval);
-					var self = this;
-					this.interval = setInterval(function() {
-						self.updateDom(0);
-						}, this.config.slideshowSpeed);						
-				}				
-                // iterate the image list index
-                this.imageIndex += 1;
-				// set flag to show stuff
-				var showSomething = true;
-                // if exceeded the size of the list, go back to zero
-                if (this.imageIndex == this.imageList.length) {
-					// if delay after last image, set to wait
-					if (this.config.delayUntilRestart > 0) {
-						this.imageIndex = -2;
-						wrapper.innerHTML = "&nbsp;";
-						showSomething = false;
-						clearInterval(this.interval);
-						var self = this;
-						this.interval = setInterval(function() {
-							self.updateDom(0);
-							}, this.config.delayUntilRestart);									
-					}
-					// if not reset index
-					else
-						this.imageIndex = 0;
-				}
-				if (showSomething) {
-					// create the image dom bit
-					var image = document.createElement("img");
-					// if set to make grayscale, flag the class set in the .css file
-					if (this.config.makeImagesGrayscale)
-						image.className = "desaturate";
-					// create an empty string
-					var styleString = '';
-					// if width or height or non-zero, add them to the style string
-					if (this.config.fixedImageWidth != 0)
-						styleString += 'width:' + this.config.fixedImageWidth + 'px;';
-					if (this.config.fixedImageHeight != 0)
-						styleString += 'height:' + this.config.fixedImageHeight + 'px;';
-					// if style string has antyhing, set it
-					if (styleString != '')
-						image.style = styleString;
-					// set the image location
-					image.src = encodeURI(this.imageList[this.imageIndex]);
-					// ad the image to the dom
-					wrapper.appendChild(image);					
-				}
-            }
-            else {
-                // if no data loaded yet, empty html
-                wrapper.innerHTML = "&nbsp;";
-            }
-        }
-        // return the dom
-		return wrapper;
-	}
+    },     
 });
+
+//------------ end -------------
